f73a79343036eae53498b30a4a355ce801bd4811
Slava Traktor <slavasobb@gmail.com>
2019-12-08 13:54:05 +0300

    update

--- client.js
+++ client.js
@@ -1,22 +1,22 @@
 var net = require('net');
 
+function sendCountToTCPPort(count, port) {
+  [...new Array(count)].forEach(() => {
+    const client = new net.Socket();
 
-
-[...new Array(15)].forEach(() => {
-  const client = new net.Socket();
-  client.connect(1235, '127.0.0.1', function() {
-    client.write('Hello, server! Love, Client.', () => {
-      client.destroy();
+    client.connect(port, '127.0.0.1', function() {
+      client.write('Hello, server! Love, Client.', () => {
+        client.destroy();
+      });
+      
+      
     });
+
+    // client.on('data', () => {
+    //   client.destroy();
+    // })
   });
-});
+}
 
 
-[...new Array(15)].forEach(() => {
-  const client = new net.Socket();
-  client.connect(1234, '127.0.0.1', function() {
-    client.write('Hello, server! Love, Client.', () => {
-      client.destroy();
-    });
-  });
-});
+sendCountToTCPPort(1, 1235);
--- server.js
+++ server.js
@@ -1,39 +1,24 @@
 var net = require('net');
 
-const tcpPorts = [
+const tcpPorts = [1234, 1235];
 
-];
+const udpPorts = [1231, 1232];
 
-const udpPorts = [
+const packageCountOnPort = new Map();
 
-];
+tcpPorts.forEach(v => {
+  packageCountOnPort.set(v, 0);
+});
 
-
-let ports = [
-  {
-    port: 1234,
-    count: 0
-  },
-  {
-    port: 1235,
-    count: 0
-  },
-]
-
-ports.forEach((v, i) => {
-  var server = net.createServer(function(socket) {
-    socket.on('data', (data) => {
-      ports[i] = {
-        ...ports[i],
-        count: ports[i].count+1
-      }
-      console.log(data.toString());
-      console.log(ports[i]);
-    })
-    socket.write('Echo server\r\n');
-    //socket.pipe(socket);
+tcpPorts.forEach((port) => {
+  const server = net.createServer(function(socket) {
+    socket.on('data', data => {
+      const oldCount = packageCountOnPort.get(port);
+      packageCountOnPort.set(port, oldCount + 1);
+      console.log(data.toString(), packageCountOnPort.get(port));
+    });
   });
 
-  console.log('listen ' + v.port);
-  server.listen(v.port, '127.0.0.1');
+  console.log('listen ' + port);
+  server.listen(port, '127.0.0.1');
 });
